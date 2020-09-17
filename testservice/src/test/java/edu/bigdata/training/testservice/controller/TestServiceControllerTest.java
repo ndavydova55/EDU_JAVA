e8bbc11ae38c32499fa12d836e50707adb6febb3
Natalya TUSHAKOVA <ndavydova55@gmail.com>
2020-09-17 12:44:29 +0300

    lab6

--- testservice/src/test/java/edu/bigdata/training/testservice/controller/TestServiceControllerTest.java
+++ testservice/src/test/java/edu/bigdata/training/testservice/controller/TestServiceControllerTest.java
@@ -11,6 +11,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
@@ -27,6 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = {ServiceConf.class, UtilsConf.class})
+@AutoConfigureMockMvc(addFilters=false)
 public class TestServiceControllerTest {
     private ObjectMapper objectMapper = new ObjectMapper();
 
